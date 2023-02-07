e3ef035579bd325492a3d83fcb5c988c8f7e77b1
jaydolphXX <justinwantsomemo@gmail.com>
2023-02-07 09:37:31 +0900

    add in/out validation

--- launch2.py
+++ launch2.py
@@ -6,21 +6,27 @@ from discord import Intents, Client, message
 token = "MTA3MjE0MTk2MjU0NDY5MzI1OA.GkqYf0.mBT5mQ-wP0gggIPGdWCn9yFJJS6LUYV9sQBrS4"
 
 intents = Intents.default()
+intents.members = True
 intents.message_content = True
 
 client = Client(intents=intents)
 
 w_data = dict()
 
+
+def initialize(ID):
+    w_data[ID] = dict()
+
+
 def office_in(message, ID):
-    if not ("in_flag" in w_data):
-        w_data[ID] = {"in_flag": True}
+    if not ("in_flag" in w_data[ID]) or (w_data[ID]["in_flag"] == False):
+        w_data[ID]["in_flag"] = True
         today = datetime.datetime.now()
         hour, minute = today.hour, today.minute
+        add_in_role(message, ID)
         add_in_count(ID)
         set_in_time(ID, today)
         update_json()
-        read_json()
 
         if (minute < 10):
             return (f"<@{message.author.id}> {hour}:0{minute} in")
@@ -29,19 +35,35 @@ def office_in(message, ID):
     else:
         return ("多重inを検知しました!")
 
+
 def add_in_count(ID):
-        if (ID in w_data):
-            w_data[ID]["in_count"] += 1
-        else:
-            w_data[ID]["in_count"] = 1
+    if ("in_count" in w_data[ID]):
+        w_data[ID]["in_count"] += 1
+    else:
+        w_data[ID]["in_count"] = 1
+
 
 def set_in_time(ID, today):
     t1 = int(datetime.datetime.timestamp(today))
     w_data[ID]["in_time"] = t1
 
 
+def update_json():
+    with open('sample.json', mode='w') as f:
+        json.dump(w_data, f, indent=4)
+        print('updated:')
+        print(w_data)
+
+
+def read_json():
+    with open('sample.json', mode='r') as f:
+        r_data = json.load(f)
+        print('read:')
+        print(r_data)
+
+
 def office_out(message, ID):
-    if ("in_flag" in w_data[ID]):
+    if ("in_flag" in w_data[ID]) and (w_data[ID]["in_flag"] == True):
         w_data[ID]["in_flag"] = False
         today = datetime.datetime.now()
         hour, minute = today.hour, today.minute
@@ -57,30 +79,37 @@ def office_out(message, ID):
         return ("まだinしていません")
 
 
-
 def add_stay_time(ID, today):
     t2 = int(datetime.datetime.timestamp(today))
     if ("stay_time" in w_data[ID]):
-            w_data[ID]["stay_time"] += t2 - w_data[ID]["in_time"]
+        w_data[ID]["stay_time"] += t2 - w_data[ID]["in_time"]
     else:
         w_data[ID]["stay_time"] = t2 - w_data[ID]["in_time"]
 
-        
-def update_json():
-    with open('sample.json', mode='w') as f:
-        json.dump(w_data, f, indent=4)
-        print('WRITE:')
-        print(w_data)
+def add_in_role(message, ID):
+    user = message.guild.get_member(ID)
+    ROLE_ID = 1040160934959783946
+    role = message.guild.get_role(ROLE_ID)
+    user.add_roles(role)
 
-def read_json():
-    with open('sample.json', mode='r') as f:
-        r_data = json.load(f)
-        print('READ')
-        print(r_data)
 
 @client.event
 async def on_ready():
+    def func():
+        for guild in client.guilds:
+            for member in guild.members:
+                yield member
     print(f"Darkey が起動しました")
+    for member in func():
+        initialize(member.id)
+    else:
+        update_json()
+
+
+@client.event
+async def on_member_join(member):
+    initialize(member.id)
+
 
 @client.event
 async def on_message(message):
